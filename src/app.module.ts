bdd61e3cf6ed825f21b78dc26c9341bbcb18595d
Mateo Letona <mateolet883@gmail.com>
2025-04-24 14:39:20 -0300

    Agrego modulo typeorm y creo entities.js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { entities } from './entities';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        hots: 'localhost',
+        database: 'asd',
+        username: 'asd',
+        password: '123'
+        synchronize: true,
+        entities,
+    }),
+    TypeOrmModule.forFeature(entities),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
