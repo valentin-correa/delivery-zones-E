781db638f123ac9029ddaed8535a449f32aae8ae
santiwg <santiwgwuri@gmail.com>
2025-04-28 00:08:17 -0300

    Make endpoint Post /Zone

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+// import { AppService } from './app.service'; puede ser que esto está de más?
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { entities } from './entities/index';
 import { DeliveryService } from './delivery/delivery.service';
 import { ZoneService } from './zone/zone.service';
+import { ZoneController } from './zone/zone.controller';
+import { DeliveryController } from './delivery/delivery.controller';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
         type: 'postgres',
-        hots: 'localhost',
+        host: 'localhost',
         database: 'asd',
         username: 'asd',
         password: '123',
@@ -19,7 +21,8 @@ import { ZoneService } from './zone/zone.service';
     }),
     TypeOrmModule.forFeature(entities),
   ],
-  controllers: [AppController],
-  providers: [AppService, DeliveryService, ZoneService],
+  controllers: [AppController, ZoneController, DeliveryController],
+  providers: [ DeliveryService, ZoneService], 
 })
 export class AppModule {}
+//le saque el AppService
