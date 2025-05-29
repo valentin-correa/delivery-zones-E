f724237f5e8a81fbfc9354672fda3d4c046d848a
Mateo Letona <mateolet883@gmail.com>
2025-05-29 14:58:01 -0300

    Create permissions to zones and delivery

--- src/delivery/delivery.controller.ts
+++ src/delivery/delivery.controller.ts
@@ -1,54 +1,75 @@
-import { Body, Controller, Get, Param, Put, Query, Post, Delete } from '@nestjs/common';
+import { Body, Controller, Get, Param, Put, Query, Post, Delete, UseGuards } from '@nestjs/common';
 import { DeliveryService } from './delivery.service';
 import {Delivery} from '../entities/deliveries.entity'
 import { FindByProximityDto, FindByZoneDTO, UpdateDeliveryStatusDto, UpdateLocationDto } from './dto/delivery.dto';
 import { CreateDeliveryDto, AssignZoneDto } from './dto/delivery.dto';
 import { Zone } from 'src/entities/zones.entity';
 import { PaginationDto } from 'src/common/dto/common.dto';
+import { AuthGuard } from 'src/middlewares/auth.middleware';
+import { Permissions } from 'src/middlewares/decorators/permissions.decorator'; 
 
 @Controller('delivery')
 export class DeliveryController {
     constructor(private readonly deliveryService: DeliveryService) {}
 
+    @UseGuards(AuthGuard)
+    @Permissions(['view-delivery'])
     @Get('findByProximity') // GET /delivery/findByProximity
     async findByProximity(@Query() findByProximityDto: FindByProximityDto):Promise<Delivery[]>{
         return await this.deliveryService.findByProximity(findByProximityDto)
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['view-delivery'])
     @Get('findByZone') // GET /delivery/findByZone
     async findByZone(@Query() zoneId: FindByZoneDTO):Promise<Delivery[]>{
         return await this.deliveryService.findByZone(zoneId);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['update-delivery'])
     @Put(':id/location') // PUT /delivery/:id/location
     async updateLocation(@Param('id') id: number, @Body() updateLocationDto: UpdateLocationDto): Promise<Delivery> {
         return await this.deliveryService.updateLocation(id, updateLocationDto);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['update-delivery'])
     @Post(':id/assignZone') // POST /delivery/:id/assignZone
     async assignZone(@Param('id') id: number, @Body() assignZoneDto: AssignZoneDto): Promise<Delivery> {
         return await this.deliveryService.assignZone(id, assignZoneDto);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['create-delivery'])
     @Post() // POST /delivery
     async createDelivery(@Body() newDelivery:CreateDeliveryDto) : Promise<Delivery>{
         return await this.deliveryService.createDelivery(newDelivery)
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delete-delivery'])
     @Delete(':id') // DELETE /delivery/:id
     async deleteDelivery(@Param('id') id:number):Promise<{ message: string }>{
         return await this.deliveryService.deleteDelivery(id)
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['update-delivery'])
     @Delete(':id/zone/:zoneId') // DELETE /delivery/:id/zone/:zoneId
     async removeZoneFromDelivery(@Param('id') deliveryId: number,@Param('zoneId') zoneId: number) {
         return await this.deliveryService.removeZoneFromDelivery(zoneId, deliveryId);
     }
-    
+
+    @UseGuards(AuthGuard)
+    @Permissions(['view-delivery'])
     @Get(':id/zones') // GET /delivery/:id/zones
     async getZonesByDelivery(@Param('id') id: number,@Query() pagination: PaginationDto) : Promise<Zone[]>{
       return this.deliveryService.findZonesByDeliveryId(id, pagination);
     }
+
+    @UseGuards(AuthGuard)
+    @Permissions(['update-delivery'])
     @Put(':id/status') // PUT /delivery/:id/status
     async updateStatus(@Param(':id') id: number, @Body() updatedStatus: UpdateDeliveryStatusDto): Promise<Delivery> {
         return await this.deliveryService.updateStatus(id, updatedStatus);
--- src/zone/zone.controller.ts
+++ src/zone/zone.controller.ts
@@ -1,38 +1,53 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, UseGuards } from '@nestjs/common';
 import { Zone } from '../entities/zones.entity';
-import { PartialUpdateZoneDto, ZoneDto } from './dto/zone.dto';
+import { PartialUpdateZoneDto, ZoneDto } from './dto/Zone.dto';
 import { ZoneService } from './zone.service';
+import { Permissions } from 'src/middlewares/decorators/permissions.decorator';
 import { PaginationDto } from 'src/common/dto/common.dto';
+import { AuthGuard } from 'src/middlewares/auth.middleware';
 
 
 @Controller('zone')
 export class ZoneController {
     constructor(private readonly zoneService: ZoneService) {}
 
+    @UseGuards(AuthGuard)
+    @Permissions(['view-zone'])
     @Get() // GET /zone
     async findAll(@Query() pagination: PaginationDto): Promise<Zone[]> {
         return this.zoneService.find(pagination);
     }
     
+    @UseGuards(AuthGuard)
+    @Permissions(['create-zone'])
     @Post () // POST /zone
     async createZone(@Body() newZone:ZoneDto): Promise<Zone>{
         return await this.zoneService.create(newZone)
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['delete-zone'])
     @Delete(':id') // DELETE /zone/:id
     async remove(@Param('id') id: number){
         return await this.zoneService.deleteZone(id);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['update-zone'])
     @Put(':id') // PUT /zone/:id
     async updateZone(@Param('id') id: number, @Body() updatedZone : ZoneDto): Promise<Zone> {
         return await this.zoneService.updateZone(id, updatedZone);
     }
 
+    @UseGuards(AuthGuard)
+    @Permissions(['view-zone'])
     @Get(':id') // GET /zone/:id
     async findById(@Param('id') id:number):Promise<Zone>{
         return await this.zoneService.findById(id)
     }
+
+    @UseGuards(AuthGuard)
+    @Permissions(['update-zone'])
     @Patch(':id') // PATCH /zone/:id
     async partialUpdate(@Param('id') id: number,@Body() updateData: PartialUpdateZoneDto): Promise<Zone> {
         return this.zoneService.partialUpdate(id, updateData);
