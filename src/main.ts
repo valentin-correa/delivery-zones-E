4c60af8f9c79e46d583b2aa582ce91892e179a14
Valentín <correavale2004@gmail.com>
2025-06-15 23:38:53 -0300

    habilito CORS para poder interactuar con el frontend

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({transform: true}));
+  app.enableCors({
+    origin: 'http://localhost:4200',
+    credentials: true
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
