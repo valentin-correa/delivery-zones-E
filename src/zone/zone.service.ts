15b7cc928ae27e97c583671b64f6752cc1be4134
santiwg <santiwgwuri@gmail.com>
2025-07-23 12:43:58 -0300

    Agrego ordenamiento por id al getZones

--- src/zone/zone.service.ts
+++ src/zone/zone.service.ts
@@ -8,10 +8,14 @@ import { take } from 'rxjs';
 
 @Injectable()
 export class ZoneService {
-  constructor(@InjectRepository(Zone) private repository: Repository<Zone>) { }
-  
-    async find(pagination: PaginationDto):Promise<Zone[]> {
-        const options: any = {}
+    constructor(@InjectRepository(Zone) private repository: Repository<Zone>) { }
+
+    async find(pagination: PaginationDto): Promise<Zone[]> {
+        const options: any = {
+            order: {
+                id: 'ASC' // ordena ascendentemente
+            }
+        }
 
         if (pagination.page !== null) {
             const offset = this.calculateOffset(pagination.page, pagination.quantity);
@@ -22,14 +26,14 @@ export class ZoneService {
         return this.repository.find(options);
     }
 
-    async create(newZone:ZoneDto): Promise<Zone>{
-        const zone=this.repository.create(newZone);
+    async create(newZone: ZoneDto): Promise<Zone> {
+        const zone = this.repository.create(newZone);
         await this.repository.save(zone);
         return zone
     }
 
     async updateZone(id: number, updateZone: ZoneDto): Promise<Zone> {
-        const zone = await this.repository.findOne( {where: {id} });
+        const zone = await this.repository.findOne({ where: { id } });
 
         if (!zone) {
             throw new NotFoundException(`Zone with id ${id} not found`);
@@ -42,48 +46,48 @@ export class ZoneService {
     }
 
     async deleteZone(id: number) {
-        const zone = await this.repository.findOne({where: {id}});
+        const zone = await this.repository.findOne({ where: { id } });
 
         if (zone) {
             await this.repository.softRemove(zone);
         }
         else throw new NotFoundException(`Zone with id ${id} not found`);
-        
+
         return { message: "Zone deleted" }
     }
-    async findById(id:number):Promise<Zone>{
+    async findById(id: number): Promise<Zone> {
         const zone = await this.repository.findOne({ where: { id } });
         if (!zone) {
             throw new NotFoundException(`Zone with id ${id} not found`);
         }
         return zone;
     }
-    
+
 
     async findZonesById(zoneIds: number[]): Promise<Zone[]> {
         return await this.repository.find(
-            {where: zoneIds.map(zoneId => ({ id: zoneId }))}
+            { where: zoneIds.map(zoneId => ({ id: zoneId })) } //Genera un array de condiciones y trae todas las entidades cuyo id está en el array"
         )
-        }
-
-    async partialUpdate(id: number, updateData: PartialUpdateZoneDto): Promise<Zone> {
-    const zone = await this.repository.findOne({ where: { id } });
-
-    if (!zone) {
-        throw new NotFoundException(`Zone with id ${id} not found`);
     }
 
-    Object.assign(zone, updateData);
+    async partialUpdate(id: number, updateData: PartialUpdateZoneDto): Promise<Zone> {
+        const zone = await this.repository.findOne({ where: { id } });
 
-    await this.repository.save(zone);
+        if (!zone) {
+            throw new NotFoundException(`Zone with id ${id} not found`);
+        }
 
-    return (await this.repository.findOne({where: {id}}))!;
-}
+        Object.assign(zone, updateData);
+
+        await this.repository.save(zone);
+
+        return (await this.repository.findOne({ where: { id } }))!;
+    }
 
     async findZonesByDeliveryId(deliveryId: number, pagination: PaginationDto): Promise<Zone[]> {
 
         const options: any = {
-            where: { deliveries: {id: deliveryId}}
+            where: { deliveries: { id: deliveryId } } //busca todas las zonas que tienen asociada un delivery con ese deliveryId.
         }
 
         if (pagination.page !== null) {
@@ -94,7 +98,7 @@ export class ZoneService {
         return await this.repository.find(options)
     }
 
-    calculateOffset(page: number, quantity: number): number{
-        return (page-1)*quantity
+    calculateOffset(page: number, quantity: number): number {
+        return (page - 1) * quantity
     }
 }
